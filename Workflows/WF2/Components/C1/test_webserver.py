175cd1f0cb40f6b90cea46faa57caf10fc5fc2ed
monkeysforever <ahlawat.randeep@gmail.com>
2020-09-09 15:49:57 -0500

    Integrated json validation into webserver and tests

--- Workflows/WF2/Components/C1/test_webserver.py
+++ Workflows/WF2/Components/C1/test_webserver.py
@@ -1,27 +1,29 @@
 import json
 import unittest
+import jsonschema
 
 from webserver import app
 
 app.testing = True
 
-class TestServer(unittest.TestCase):
+class TestServer(unittest.TestCase):    
     def test_correct_order(self):
         with app.test_client() as client:
-            with open('./correct-orders.json') as correct_orders:
-                orders = json.load(correct_orders)
+            with open("./correct-orders.json", "r") as correct_orders:                
+                orders = json.loads(correct_orders.read())
             for key in orders:
                 order = orders[key]
                 data = json.dumps(order)
-                result = client.post('/order', json=data)
+                result = client.post('/order', json=data)            
                 self.assertEqual('200 OK', result.status)
 
     def test_incorrect_order(self):
-        with app.test_client() as client:
-            with open('./incorrect-orders.json') as incorrect_orders:
-                orders = json.load(incorrect_orders)
+        with app.test_client() as client:            
+            with open("./incorrect-orders.json", "r") as incorrect_orders:
+                orders = json.loads(incorrect_orders.read())
             for key in orders:
                 order = orders[key]
                 data = json.dumps(order)
-                result = client.post('/order', json=data)
-                self.assertEqual('400 BAD REQUEST', result.status)
+                result = client.post('/order', json=data)            
+                self.assertEqual('400 BAD REQUEST', result.status)     
+
--- Workflows/WF2/Components/C1/webserver.py
+++ Workflows/WF2/Components/C1/webserver.py
@@ -1,30 +1,28 @@
 from flask import Flask, request, Response
-
+import jsonschema
 import json
 
 app = Flask(__name__)
 
-def verify_order(order):
-    items = order['items']
-    for key in items:
-        if items[key] < 0:
-            return False
-    return True
+schema = None
 
-@app.route('/order', methods=['POST'])
-def recieve_order():
-    data = request.get_json()
-    order = json.loads(data)
-    if verify_order(order):
-        # Initiate Workflow
-        # Add order to DB
-        return Response(response=json.dumps(order),
-                status=200,
-                mimetype='application/json')
-    else:
-        return Response(response=json.dumps(order),
+def verify_order(order, schema):
+    try:
+        jsonschema.validate(instance=order, schema=schema)
+    except:
+        return Response(response=json.dumps(order), 
                 status=400,
                 mimetype='application/json')
+    
+    return Response(response=json.dumps(order), 
+                status=200,
+                mimetype='application/json')
 
-if __name__ == "__main__":
-    app.run()
+
+@app.route('/order', methods=['POST'])
+def main():    
+    data = request.get_json()
+    with open("./schema.json", "r") as schema:
+        schema = json.loads(schema.read())
+    order = json.loads(data)  
+    return verify_order(order, schema)
