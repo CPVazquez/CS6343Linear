bafce67a8a4371369c7e21f32cc615fa9518f054
monkeysforever <ahlawat.randeep@gmail.com>
2020-09-17 19:29:20 -0500

    Added best delivery entity selection

--- Workflows/WF2/Components/C3/src/get_delivery_time.py
+++ Workflows/WF2/Components/C3/src/get_delivery_time.py
@@ -1,12 +1,12 @@
 import requests
 import json
 from config import API_KEY
+import heapq
  
 URL = "https://maps.googleapis.com/maps/api/directions/json?origin={}&destination={}&key={}"
 
 def _convert_time_str(time):
-    time = time.split()    
-    print(time)
+    time = time.split()        
     if len(time) > 2:
         mins = int(time[2])
         hours = int(time[0])
@@ -15,18 +15,32 @@ def _convert_time_str(time):
         hours = 0
     return hours * 60 + mins    
 
-def get_time(origin, destination):
-    origin = ",".join(map(str, origin))
-    destination = ",".join(map(str, destination))
+def _get_time(origin, destination):
     url = URL.format(origin, destination, API_KEY)    
     response = requests.get(url)
     content = json.loads(response.content.decode())    
     time = (content['routes'][0]['legs'][0]['duration']['text'])        
     return _convert_time_str(time)
 
+def get_delivery_time(origin, destination, delivery_entities):
+    origin = ",".join(map(str, origin))
+    destination = ",".join(map(str, destination))
+    delivery_entities = [",".join(map(str, coordinates)) for coordinates in delivery_entities]    
+
+    best_time = float('inf')
+    best_entity = 0
+    for idx, delivery_entity in enumerate(delivery_entities):        
+        time = _get_time(delivery_entity, origin)                
+        if time < best_time:
+            best_time = time
+            best_entity = idx
+
+    return _get_time(origin, destination) + best_time, best_entity
+
 if __name__ == "__main__":
-    origin = (32.9982, -96.7756)
-    destination = (32.9858, -96.7501)    
-    print(get_time(origin, destination))
+    store = (32.984363, -96.749689) #utd
+    customer = (32.998323, -96.775618) #pearl on frankford
+    delivery_entities = [(32.993394, -96.768680), (32.998795, -96.734466), (32.988504, -96.770228)] #palencia, marquis, chatham
+    print(get_delivery_time(store, customer, delivery_entities))
         
     
