9e78a2dc0003cc1afe94f24c39fd3c873bd25958
Carla Vazquez <trishaire@Carlas-MBP.lan>
2020-09-25 08:12:52 -0500

    fixed connection issues for custom cassandra container

---
+++ Workflows/WF2/Components/C2/src/entry-wrap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for f in /opt/data/*; do
+    case "$f" in
+        *.sh)     echo "$0: running $f"; . "$f" ;;
+        *.cql)    echo "$0: running $f" && until cqlsh -f "$f"; do >&2 echo "Cassandra is unavailable - sleeping"; sleep 10; done & ;;
+        *)        echo "$0: ignoring $f" ;;
+    esac
+    echo
+done
+exec /docker-entrypoint.sh "$@"
