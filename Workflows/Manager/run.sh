260577bfb4e9b74a49ce900d0a45344137c414dc
Daniel Garcia <cryhavoc24@gmail.com>
2020-09-26 15:11:37 -0500

    added more api functionality

---
+++ Workflows/Manager/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export FLASK_APP=src/test.py
+flask run 
+
--- Workflows/Manager/src/test.py
+++ Workflows/Manager/src/test.py
@@ -1,10 +1,19 @@
 import docker
 client = docker.from_env()
 
+def get_network_by_name(network_name):
+    return next( x for x in client.networks.list() if x.name == network_name )
+
+overlay_network = get_network_by_name("mynetwork")
+
 from flask import Flask, request, Response
 app = Flask(__name__)
 
-@app.route('/dockerize', methods=['POST'])
+@app.route('/dockerize/networks', methods=['GET', 'POST'])
+def dockerize_networks_function():
+    return Response(status=200,response=overlay_network.name)
+
+@app.route('/dockerize', methods=['GET', 'POST'])
 def dockerize_function():
     containers_list = client.containers.list()
     to_sendback = "["
@@ -12,10 +21,9 @@ def dockerize_function():
         to_sendback += i.name 
         to_sendback += " " 
     to_sendback += "]"
-
     return Response(status=200,response=to_sendback)
 
-@app.route('/health', methods=['POST'])
+@app.route('/health', methods=['GET', 'POST'])
 def health_check():
     return Response(status=200,response="healthy")
 
