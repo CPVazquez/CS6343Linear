6a9b3a3e68c49e377b5a9a5c80e98817e0ba8928
Carla Vazquez <trishaire@Carlas-MacBook-Pro.local>
2020-09-26 13:56:32 -0500

    restocker working

--- Workflows/WF2/Components/C5/src/restocker.py
+++ Workflows/WF2/Components/C5/src/restocker.py
@@ -3,13 +3,20 @@ from cassandra.cluster import Cluster
 import jsonschema
 import json
 import uuid
+import time
+import threading
+import logging
 
 cluster = Cluster()
-# session = cluster.connect('pizza_grocery')
-# add_stock_prepared = session.prepare('UPDATE stock SET quantity = ?  WHERE store = ? AND itemName = ?')
+session = cluster.connect('pizza_grocery')
+get_quantity = session.prepare('SELECT quantity FROM stock  WHERE storeID = ? AND itemName = ?')
+add_stock_prepared = session.prepare('UPDATE stock SET quantity = ?  WHERE storeID = ? AND itemName = ?')
+get_stores = session.prepare("SELECT storeID FROM stores")
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
 
 app = Flask(__name__)
-
 
 with open("src/restock-order.schema.json", "r") as schema:
     schema = json.loads(schema.read())
@@ -31,20 +38,35 @@ def verify_restock_order(order):
 
 @app.route('/restock', methods=['POST'])
 def restocker():
+
     valid = False
     restock_json = request.get_json(silent=True)
+    response = Response(status=400, response="Restocking order ill formated.\nRejecting request.\nPlease correct formating")
 
     
     if restock_json != None :
         valid = verify_restock_order(restock_json)
 
         if valid :
-            storeID = uuid.UUID(restock_dictionary["storeID"])
-            for item_dict in restock_dictionary["restock-list"]:
-                pass#    pass  session.execute(add_stock_prepared, item_dict["quantity"], storeID, item_dict["item-name"])
+            storeID = uuid.UUID(restock_json["storeID"])
+            for item_dict in restock_json["restock-list"]:
+                session.execute(add_stock_prepared, (item_dict["quantity"], storeID, item_dict["item-name"]))
+            response = Response(status=200, response="Filled out the following restock order: \n" + json.dumps(restock_json))
 
-    return Response(status=200, response="1" if valid else "0")
-        
+    return response
 
 
-        
+# #scan the database for items that are out of stock or close to it
+# def scan_out_of_stock():
+#     stores = session.execute(get_stores)
+#     for store in stores:
+#         logging.debug("Scanning store: " + str(store.storeid) + " for out of stock items")
+#         out_of_stock = session.execute(scan_prepared, (store.storeid, ))
+#         for row in out_of_stock:
+#             logging.debug(str(row.storeid) + " out of " + row.itemname)
+#         out_of_stock = None
+#         #session.execute(add_stock_prepared, (4.0, store.storeid, "Bacon"))
+    
+#     threading.Timer(500, scan_out_of_stock).start()
+
+# scan_out_of_stock()
