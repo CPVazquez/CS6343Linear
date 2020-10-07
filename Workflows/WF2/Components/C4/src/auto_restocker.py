d71ddb7cd68d7060fe061c104b2a1614ecfe1f3f
Randeep <rsa190000@cluster1-1.utdallas.edu>
2020-10-07 14:34:48 -0500

    Added flask to component

--- Workflows/WF2/Components/C4/src/auto_restocker.py
+++ Workflows/WF2/Components/C4/src/auto_restocker.py
@@ -1,4 +1,3 @@
-import os
 import logging
 import uuid
 
@@ -17,16 +16,19 @@ __status__ = "Development"
 
 '''Component for forecasting the demand of an item and automatically restocking'''
 
+#Flask application initialzation
+app = Flask(__name__)
+
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
 
 logger = logging.getLogger(__name__)
 
-#Connecting to Cassandra Cluster
-    
+#Connecting to Cassandra Cluster    
 cluster = Cluster(['cass'])
 session = cluster.connect('pizza_grocery')
 
+#Prepared Queries
 get_item_stock_query = session.prepare("Select * from stockTracker where storeID=? and itemName=? and dateSold<=?")
 get_current_stock_query = session.prepare("Select quantity from stock where storeID=? and itemName=?")
 update_stock_query = session.prepare("Update stock set quantity=? where storeID=? and itemName=?")
@@ -58,31 +60,65 @@ def _predict_item_stocks(store_id, item_name, history, days):
 	df = rows._current_rows
 	df = df[['datesold','quantitysold']].sort_values(['datesold'], ascending=False)	
 	df = df.iloc[:history, :]
-	df = df.rename(columns={'datesold':'ds', 'quantitysold':'y'})
-	logger.info('Training Data: {}'.format(df))
+	df = df.rename(columns={'datesold':'ds', 'quantitysold':'y'})	
 	m = Prophet()
 	m.fit(df)
 	future = m.make_future_dataframe(periods=days, freq='d', include_history=False)
 	forecast = m.predict(future)
-	prediction = forecast['yhat'].tolist()
-	
+	prediction = forecast['yhat'].tolist()	
 	logger.info('Precdiction:: Date: {}, Quantity: {}'.format(forecast['ds'],sum(prediction)))
 	return prediction
 
 
-def auto_restock(store_id, item_id, history, days):
-	stock = _get_current_stock(store_id, item_id)
-	predictions = _predict_item_stocks(store_id, item_name, history, days)
+def auto_restock(store_id, item_name, history, days):
+	'''Forecasts the demand for a item in a store using previous sale data and automatically restocks.
+
+		Parameters:
+			store_id (UUID): ID of the store
+			item_name (string): Name of the item
+			history (int): Number of days of sale data to be used 
+			days (int): Number of forecasted days
+		Returns:
+			Response (object): Response object for POST request
+	'''
+
+	try:
+		stock = _get_current_stock(store_id, item_name)
+	except:
+		return Response(status=400, response="Invalid store id or item id")
+	try:
+		predictions = _predict_item_stocks(store_id, item_name, history, days)
+	except:
+		 return Response(status=500, reponse="Facebook Prophet error")
+
 	total_stock = sum(predictions)
 	if stock < total_stock:
 		_update_stock(store_id, item_name, total_stock)
 		logger.info('Updated stock of Item: {} in Store: {} to {}'.format(item_name, store_id, total_stock))
 	else:
-		logger.info('Stock Surplus of {} available for Item: {} in Store: {}'.format(stock, item_name, store_id))
-	
-	
+		logger.info('Stock Surplus of {} available for Item: {} in Store: {}'.format(stock,
+			item_name, store_id))
 
-if __name__ == '__main__':
+
+@app.route('/auto-restock', methods=['POST'])
+def restock():
+	'''REST API for auto-restocking an item in a store.'''
+
+	data = json.loads(request.get_json())
+	store_id = uuid.UUID(data['store_id'])
+	item_name = data['item_name']
+	history = data['history']
+	days = data['days']
+	return auto_restock(store_id, item_name, history, days)	
+
+	
+@app.route('/health', methods=['GET'])
+def health_check():
+	'''REST API for checking health of task.'''
+
+	return Response(status=200,response="healthy")
+
+def _test():
 	store_id = uuid.UUID('7098813e-4624-462a-81a1-7e0e4e67631d')
 	item_name = "Dough"
 	auto_restock(store_id, item_name, 5, 1)
