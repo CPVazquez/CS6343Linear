15c251391a93713d0fb73ed7811eb44167b10175
CPVazquez <cpv150030@cluster1-1.utdallas.edu>
2020-10-08 10:17:43 -0500

    made a health check for cassandra

--- Workflows/WF2/Components/C2/tests/healthy_test.py
+++ Workflows/WF2/Components/C2/tests/healthy_test.py
@@ -1,18 +1,26 @@
 import docker
 
 import logging
+import json
 from time import sleep
 
 healthy = False
-client = docker.APIClient(base_url='unix://var/run/docker.sock')
+APIclient = docker.APIClient(base_url='unix://var/run/docker.sock')
 logging.basicConfig(level=logging.DEBUG, 
     format='%(asctime)s - %(levelname)s - %(message)s')
-
+client = docker.from_env()
 while not healthy : 
-    result = client.inspect_container("cass")['State']['Health']['Status']
-    if result == 'healthy':
-        healthy = True
-    else:
+    tasks = client.services.get("cass").tasks()
+    #logging.debug(json.dumps(tasks,sort_keys=True, indent=4))
+    
+    for task in tasks:
+        tID = task['ID']
+        result = APIclient.inspect_task(tID)['Status']['Message']
+        if result == 'started':
+            healthy = True    
+    
+
+    if not healthy:
         logging.debug("Cass failed health check")
         sleep(5)
 
