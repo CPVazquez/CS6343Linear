f07981d376660c603113b84e9cab7dcfac08bd3f
Carla Vazquez <trishaire@Carlas-MacBook-Pro.local>
2020-10-08 10:36:16 -0500

    cleaned up healthytest

--- Workflows/WF2/Components/C2/tests/healthy_test.py
+++ Workflows/WF2/Components/C2/tests/healthy_test.py
@@ -4,26 +4,25 @@ import logging
 import json
 from time import sleep
 
-healthy = False
-APIclient = docker.APIClient(base_url='unix://var/run/docker.sock')
 logging.basicConfig(level=logging.DEBUG, 
     format='%(asctime)s - %(levelname)s - %(message)s')
+
+healthy = False
+
 client = docker.from_env()
+APIclient = docker.APIClient(base_url='unix://var/run/docker.sock')
+
 while not healthy : 
     tasks = client.services.get("cass").tasks()
-    #logging.debug(json.dumps(tasks,sort_keys=True, indent=4))
-    
+
     for task in tasks:
         tID = task['ID']
         result = APIclient.inspect_task(tID)['Status']['Message']
         if result == 'started':
             healthy = True    
     
-
     if not healthy:
         logging.debug("Cass failed health check")
         sleep(5)
 
 logging.debug('Cass is healthy!')
-
-#docker inspect --format='{{json .State.Health}}' cass 
