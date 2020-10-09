466f180609c231e3d9244e7deb53e4c4cf1d8051
Randeep <rsa190000@cluster1-1.utdallas.edu>
2020-10-08 21:42:35 -0500

    fixed json

--- Workflows/WF2/Components/C3/src/delivery_assigner.py
+++ Workflows/WF2/Components/C3/src/delivery_assigner.py
@@ -148,7 +148,7 @@ def assign():
     '''REST API for assigning best delivery entity.'''
 
     data = request.get_json()
-    order_id = uuid.UUID(json.loads(data)['order_id'])
+    order_id = uuid.UUID(data['order_id'])
     return assign_entity(order_id)
 
 
