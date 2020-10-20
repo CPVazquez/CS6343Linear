cddb81bc1f079f74f223cd1c570f3d60ebe62dcb
Carla Vazquez <trishaire@Carlas-MacBook-Pro.local>
2020-10-20 09:30:13 -0500

    removed threading for async thing

--- Workflows/Manager/src/wkf-manager.py
+++ Workflows/Manager/src/wkf-manager.py
@@ -112,12 +112,7 @@ def start_cass(workflow_json, response_list):
                 logging.debug("cass is not ready")
                 message = "Attempting to spin up cass"
                 message_dict = {"message": message}
-                # x = threading.Thread(
-                #     target=requests.post,
-                #     args=(origin_url, None, json.dumps(message_dict)),
-                #     daemon=True
-                # )
-                # x.start()
+                requests.post(origin_url, None, json.dumps(message_dict))
                 sleep(5)
                 count += 5
             else:  # request timed out
