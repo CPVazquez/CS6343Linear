de6015813b8f3db620fd5611a98e9aa2741100bb
Carla Vazquez <trishaire@Carlas-MacBook-Pro.local>
2020-10-21 16:27:35 -0500

    changing schema primary keys and back to schema.sql

--- Workflows/WF2/Components/C2/src/entry-wrap.sh
+++ Workflows/WF2/Components/C2/src/entry-wrap.sh
@@ -7,7 +7,7 @@
 # status = "Development"
 
 if [ ! -d "$CASSANDRA_HOME/data/data/pizza_grocery" ] || [ -z "$(ls $CASSANDRA_HOME/data/data/pizza_grocery)" ]; then
-    echo "$0: running /opt/data/pizza.cql" && until cqlsh -f opt/data/pizza.cql; do >&2 echo "Cassandra is unavailable - sleeping"; sleep 2; done &
+    echo "$0: running /opt/data/schema.cql" && until cqlsh -f opt/data/schema.cql; do >&2 echo "Cassandra is unavailable - sleeping"; sleep 2; done &
 fi
 
 exec /docker-entrypoint.sh "$@"
