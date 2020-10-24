01d42c34a36a9a0bb9152e48ce2437fb9a6d34c7
CPVazquez <cpv150030@cluster1-1.utdallas.edu>
2020-10-24 11:41:48 -0500

    changed wkf from a service to a containter

--- Workflows/Manager/service.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-
-docker service create --name wkf-manager --env TIMEOUT='1000' --publish 8080:8080 --mount 'type=bind,src=/root/.docker/config.json,dst=/root/.docker/config.json' --mount 'type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock' --constraint=node.hostname==cluster1-1.utdallas.edu --network myNet trishaire/wkf-manager 
-
