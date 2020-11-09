fa896d87269ee997c40e2467bce306dbd7b4b56f
Chris Scott <cms190009@utdallas.edu>
2020-11-08 19:20:56 -0600

    Update generate_order.py

--- Workflows/WF2/Order/generate_order.py
+++ Workflows/WF2/Order/generate_order.py
@@ -96,7 +96,7 @@ def get_component_urls(store_id):
     wkf_manager_url = cluster_url + ":8080/workflow-requests/" + store_id
     response = requests.get(wkf_manager_url)
     print(str(response.status_code) + ", " + response.text + "\n")
-    if response.status_code != 200:
+    if response.status_code == 400:
         print("ERROR: " + response.text)
         print("Script is terminating...")
         exit()
