c5483961a11513f6e0f48e84b907a0e61387140f
Chris Scott <cms190009@utdallas.edu>
2020-11-09 14:22:38 -0600

    replace auto-restocker with predictor

--- Workflows/WF2/Components/C6/src/order-processor.py
+++ Workflows/WF2/Components/C6/src/order-processor.py
@@ -179,7 +179,7 @@ def get_component_url(component, store_id):
     url = "http://" + comp_name + ":"
     if component == "delivery-assigner":
         url += "3000/order"
-    elif component == "auto-restocker" or component == "predictor":
+    elif component == "predictor":
         url += "4000/order"
     elif component == "restocker":
         url += "5000/order"
