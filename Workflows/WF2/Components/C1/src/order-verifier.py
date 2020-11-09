10c41b994c9363807ed80d155048f67a39dd7bdb
Chris Scott <cms190009@utdallas.edu>
2020-11-09 14:42:30 -0600

    added order-processor to workflow-request.schema.json in C1, C2, Manager

--- Workflows/WF2/Components/C1/src/order-verifier.py
+++ Workflows/WF2/Components/C1/src/order-verifier.py
@@ -53,7 +53,7 @@ def get_component_url(component, store_id):
     url = "http://" + comp_name + ":"
     if component == "delivery-assigner":
         url += "3000/order"
-    elif component == "predictor":
+    elif component == "auto-restocker":
         url += "4000/order"
     elif component == "restocker":
         url += "5000/order"
