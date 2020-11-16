2e91d7ebafe176a87b8076417210525043d96918
Chris Scott <cms190009@utdallas.edu>
2020-11-16 13:06:33 -0600

    change requests log level to WARNING

--- Workflows/WF2/Components/C1/src/order-verifier.py
+++ Workflows/WF2/Components/C1/src/order-verifier.py
@@ -33,7 +33,7 @@ logging.basicConfig(
     level=logging.INFO, 
     format='%(asctime)s - %(levelname)s - %(message)s'
 )
-# logging.getLogger('requests').setLevel(logging.INFO)
+logging.getLogger('requests').setLevel(logging.WARNING)
 
 # Global workflows dict
 workflows = dict()
