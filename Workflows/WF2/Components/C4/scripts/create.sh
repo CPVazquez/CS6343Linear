d7b13a7ebc12aaba6c385c2fcc17f62a5f57b4a6
Randeep <rsa190000@cluster1-1.utdallas.edu>
2020-11-17 03:04:49 -0600

    Dockerfile

--- Workflows/WF2/Components/C4/scripts/create.sh
+++ Workflows/WF2/Components/C4/scripts/create.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker service create --name stock-analyzer --publish 4000:4000 \
-	--network myNet --env CASS_DB=cass1 trishaire/stock-analyzer:linear
+	--network myNet --env CASS_DB=cass trishaire/stock-analyzer:linear
